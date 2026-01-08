37ef1db1b76a9e9a728918315ecefbbd7c073ab2
VACHAN SHETTY <vachan316@gmail.com>
2026-01-08 19:34:18 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
